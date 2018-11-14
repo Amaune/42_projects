d46a7de139a40a0051fc52000741ca6de0e3ec19
Amaury Aune <amaune@z3r2p1.le-101.fr>
2018-11-14 15:49:47 +0100

    libft

---
+++ libft/src/ft_lstdel.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                          LE - /            */
+/*                                                              /             */
+/*   ft_lstdel.c                                      .::    .:/ .      .::   */
+/*                                                 +:+:+   +:    +:  +:+:+    */
+/*   By: amaune <marvin@le-101.fr>                  +:+   +:    +:    +:+     */
+/*                                                 #+#   #+    #+    #+#      */
+/*   Created: 2018/10/09 17:19:10 by amaune       #+#   ##    ##    #+#       */
+/*   Updated: 2018/10/10 15:41:59 by amaune      ###    #+. /#+    ###.fr     */
+/*                                                         /                  */
+/*                                                        /                   */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	ft_lstdel(t_list **alst, void (*del)(void *, size_t))
+{
+	t_list *ptr;
+
+	while (*alst != NULL)
+	{
+		ptr = (*alst)->next;
+		ft_lstdelone(alst, del);
+		*alst = ptr;
+	}
+	*alst = NULL;
+}
